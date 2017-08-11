f5b0809f6c796ad816e0b2a9b3a976945aa2512b
ehime <dodomeki@gmail.com>
2017-08-11 12:23:11 -0700

    adding populator fix and rsa keys

--- scripts/populate_sshkey.sh
+++ scripts/populate_sshkey.sh
@@ -5,9 +5,9 @@ USER=$2
 
 sudo -u $USER mkdir -p $HOME/.ssh
 sudo -u $USER chmod 700 $HOME/.ssh
-sudo -u $USER cp /vagrant/scripts/sshkey/id_dsa $HOME/.ssh/id_dsa
-sudo -u $USER chmod 600 $HOME/.ssh/id_dsa
-sudo -u $USER cat /vagrant/scripts/sshkey/id_dsa.pub >> $HOME/.ssh/authorized_keys
+sudo -u $USER cp /vagrant/scripts/sshkey/id_rsa $HOME/.ssh/id_rsa
+sudo -u $USER chmod 600 $HOME/.ssh/id_rsa
+sudo -u $USER cat /vagrant/scripts/sshkey/id_rsa.pub >> $HOME/.ssh/authorized_keys
 sudo -u $USER chmod 600 $HOME/.ssh/authorized_keys
 cat << EOT > $HOME/.ssh/config
 host *
